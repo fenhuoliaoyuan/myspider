a1012ba1d60963423d1fdf4c773950863067d377
fenhuoliaoyuan <72696691+fenhuoliaoyuan@users.noreply.github.com>
2021-06-14 01:47:11 +0800

    Add files via upload

--- B站视频下载.py
+++ B站视频下载.py
@@ -5,11 +5,18 @@ import requests
 
 # https://space.bilibili.com/8366990/video?tid=0&page=4&keyword=&order=pubdate
 URL_0 = input('例如https://space.bilibili.com/8366990/video?tid=0&page=4&keyword=&order=pubdate\n输入你要爬取的用户视频列表地址 :   ')
+if 'page' not in URL_0:
+    URL_0_0 = URL_0.split('?')
+    URL_0 = URL_0_0[0] + '?' + 'page=1&' + URL_0_0[1]
 URL_1 = list(re.compile('(.*)page=\d(&.*)').findall(URL_0)[0])
 start_page = input('输入你要爬取的开始页数： ')
 end_page = input('输入你要爬取的截至页数： ')
 # 切换目录 r'E:\B站视频下载\'
-os.chdir(input("输入你要存储视频的目录路径 ："))
+# os.chdir(input("输入你要存储视频的目录路径 ："))
+dir_name = input("输入你要存储视频的目录路径 ：")
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+os.chdir(dir_name)
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
 }
