c13a3d046b47f1365a453b0364f2797774ff08aa
fenhuoliaoyuan <b2319423737@gmail.com>
2022-04-08 19:10:08 +0800

    添加下载单个推文视频或者图片的功能

--- twitter.py
+++ twitter.py
@@ -1,11 +1,15 @@
 import json
 import os
+import pprint
 import random
 from urllib.parse import quote
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from configTwitter import *
+
 video_updates = []
+
+
 class TwitterMediaDownload(object):
     data_list = []
 
@@ -190,7 +194,7 @@ class TwitterMediaDownload(object):
 
 def format_str(str_mabge):
     return str_mabge.replace('.', ' ').replace('?', ' ').replace('\n', ' ').replace('|', ' ').replace(':', ' ').replace(
-        '!', ' ').replace('/', ' ').replace('\\', ' ').replace('*', ' ').replace('<', ' ').replace('>', ' ')
+        '!', ' ').replace('/', ' ').replace('\\', ' ').replace('*', ' ').replace('<', ' ').replace('>', ' ').strip()
 
 
 def downloadInf(user_id, path_user):
@@ -286,16 +290,152 @@ def add_user():
             TwitterMediaDownload.data_list = []
             downloadInf(user_id=user_id, path_user=path_user)
 
+def parse_legacy(legacy):
+    datasList = []
+    try:
+        videoTitle = legacy['full_text']
+        videoTitle = videoTitle.split('https')[0].replace('\n', '')
+        # print(videoTitle)
+    except:
+        videoTitle = None
+    try:
+        videoUrl = ''
+        videoUrls = legacy['extended_entities']['media'][0]['video_info']['variants']  # [1]['url']
+        videoUrls = [i for i in videoUrls if i['content_type'] == 'video/mp4']
+        videoUrls = sorted(videoUrls, key=lambda e: e['bitrate'], reverse=True)
+        videoUrl = videoUrls[0]['url']
+        # print(videoUrl)
+        # for row i
+        # n videoUrls:
+        #     if row['content_type'] == 'video/mp4':
+        #         videoUrl = row['url']
+
+    except:
+        videoUrl = None
+    if videoUrl is not None:
+        data = {
+            'videoTitle': videoTitle,
+            'videoUrl': videoUrl
+        }
+        datasList.append(data)
+
+    ##########################################################
+    try:
+        photoTitle_ = legacy['full_text']
+        photoTitle = photoTitle_.split('https')[0].replace('\n', '')
+        if len(photoTitle) < 1:
+            photoTitle = photoTitle_.split('/')[-1].replace('\n', '')
+        # print(photoTitle)
+    except:
+        photoTitle = None
+    try:
+        # photoUrls = legacy['entities']['media']
+        # 使用extended_entities更好些
+        photoUrls = legacy['extended_entities']['media']
+        # photoUrl = legacy['entities']['media'][0]['media_url_https']
+    except:
+        photoUrls = None
+    if len(photoTitle) > 0 and len(photoUrls) > 0:
+        if len(photoUrls) > 1:
+            for i in photoUrls:
+                # 排除抓取到视频封面的情况
+                keys = list(i.keys())
+                if 'video_info' not in keys:
+                    photoUrl = i['media_url_https']
+                    photoTitle = photoTitle + ' ' + photoUrl.split('/')[-1].replace('.jpg', '')
+                    data = {
+                        'photoTitle': photoTitle,
+                        'photoUrl': photoUrl
+                    }
+                    datasList.append(data)
+                    photoTitle = photoTitle.replace(
+                        ' ' + photoUrl.split('/')[-1].replace('.jpg', ''),
+                        '')
+        else:
+            if 'video_info' not in list(photoUrls[0].keys()):
+                photoUrl = photoUrls[0]['media_url_https']
+                data = {
+                    'photoTitle': photoTitle,
+                    'photoUrl': photoUrl
+                }
+                datasList.append(data)
+    return datasList
+def download_one():
+    url = input('输入你要下载的视频或图片所在网页地址：')
+    path_user = input('输入你的存储目录：')
+    focalTweetId = url.split('/')[-1]
+    params_detail = {"focalTweetId": focalTweetId,
+                     "with_rux_injections": False,
+                     "includePromotedContent": True,
+                     "withCommunity": True,
+                     "withQuickPromoteEligibilityTweetFields": True,
+                     "withBirdwatchNotes": False,
+                     "withSuperFollowsUserFields": True,
+                     "withDownvotePerspective": False,
+                     "withReactionsMetadata": False,
+                     "withReactionsPerspective": False,
+                     "withSuperFollowsTweetFields": True,
+                     "withVoice": True,
+                     "withV2Timeline": True,
+                     "__fs_dont_mention_me_view_api_enabled": True,
+                     "__fs_interactive_text_enabled": True,
+                     "__fs_responsive_web_uc_gql_enabled": False}
+
+    url_detail_inf = f'https://twitter.com/i/api/graphql/x67pmns2hNpQZHBOHuGBBQ/TweetDetail?variables={quote(json.dumps(params_detail, separators=(",", ":")))}'
+    headers = {
+        'accept': '*/*',
+        # 'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9,ja;q=0.8',
+        'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+        'content-type': 'application/json',
+        'cookie': 'kdt=SNua313K5dXV5Tpj0hmADRf5AK1AgaM7vBItF02t; remember_checked_on=1; guest_id_marketing=v1%3A162211230601704388; guest_id_ads=v1%3A162211230601704388; lang=zh-cn; dnt=1; personalization_id="v1_vgqD3xek7JXRMdh0TemIlw=="; guest_id=v1%3A164653051595729144; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLju3Vx%252FAToMY3NyZl9p%250AZCIlYTYzZjExM2U2MjdkMGY5NzMxYWIwYTA5ODE5NmUwMTk6B2lkIiVmZWQ0%250ANjkxNmU3YTI2Y2RmNzQxNmY5N2IzMmY4ZWQ1MQ%253D%253D--1a6e87c3cf286b07b59e5889659b7a8534fa6a84; g_state={"i_l":1,"i_p":1646537827556}; auth_token=e05d22cfc8f27f151480a9c88ce11e3ac0a4a274; ct0=02d7ba522df0f1d306d146c80c4785f506ae62e5580b436b1a1aa292673ee67fba2203fef79a35af8b121cfe09bfcdb92d5b037e3d6cf0632eafd724223e771fe91227946b8091ce1247e08acc7051a0; twid=u%3D1254983364622180352; at_check=true; mbox=session#0d30c14b97c049f287eee5b061d455ca#1647425340|PC#0d30c14b97c049f287eee5b061d455ca.38_0#1710668280; _ga_BYKEBDM7DS=GS1.1.1647423485.1.0.1647423485.0; _ga=GA1.2.1554362123.1636996095; _gid=GA1.2.242504478.1649393409',
+        'referer': url,
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+        'x-csrf-token': '02d7ba522df0f1d306d146c80c4785f506ae62e5580b436b1a1aa292673ee67fba2203fef79a35af8b121cfe09bfcdb92d5b037e3d6cf0632eafd724223e771fe91227946b8091ce1247e08acc7051a0',
+        'x-twitter-active-user': 'yes',
+        'x-twitter-auth-type': 'OAuth2Session',
+        'x-twitter-client-language': 'zh-cn',
+    }
+    res = requests.get(url=url_detail_inf,headers=headers)
+    datasList = []
+    if res.status_code == 200:
+        res = res.json()
+        try:
+            legacy = res['data']['threaded_conversation_with_injections_v2']['instructions'][0]['entries'][0]['content']['itemContent']['tweet_results']['result']['legacy']
+
+        except:
+            pass
+            # legacy = data.threaded_conversation_with_injections_v2.instructions[0].entries[0].content.itemContent.tweet_results.result.legacy
+        datasList = parse_legacy(legacy=legacy)
+        if len(datasList) > 0:
+            # dict的去重
+            datasList = [dict(t) for t in set([tuple(d.items()) for d in datasList])]
+            # 插入路径
+            for i in datasList:
+                i['path_user'] = path_user
+            print("抓取的视频/图片列表长度：" + str(len(datasList)))
+            print('开始下载视频/图片>>>')
+            with ThreadPoolExecutor(1) as tp:
+                for data in datasList:
+                    tp.submit(TwitterMediaDownload.saveToFile, data)
 
 def main():
     while True:
-        num = input('选择添加目标用户请输入1\n选择更新已有个别用户视频请输入2\n选择更新已有所有用户视频请输入3\n选择退出请输入q\n')
+        num = input('选择添加目标用户请输入1\n选择更新已有个别用户视频请输入2\n选择更新已有所有用户视频请输入3\n选择下载单个视频或图片请输入4\n选择退出请输入q\n')
         if num == '1':
             add_user()
         elif num == '2':
             updateOneOf()
         elif num == '3':
             update()
+        elif num == '4':
+            download_one()
         elif num == 'q':
             break
         else:
